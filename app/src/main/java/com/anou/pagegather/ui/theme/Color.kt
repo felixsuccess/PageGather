b51bff65536c593147fa1db95293772dc78cddbb
ajf <ajf@anou.tech>
2025-05-12 15:05:58 +0800

    feat(theme): 更新应用主题颜色
    - 移除旧的主题颜色，添加新的主色调、辅助色调、强调色等颜色定义
    - 更新深色和浅色主题的配色方案
    - 增加更多颜色以支持不同的UI元素和状态

--- app/src/main/java/com/anou/pagegather/ui/theme/Color.kt
+++ app/src/main/java/com/anou/pagegather/ui/theme/Color.kt
@@ -2,10 +2,37 @@ package com.anou.pagegather.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// 主色调
+val PrimaryBlueDark = Color(0xFF1A237E)
+val PrimaryBlueLight = Color(0xFF536DFE)
+
+// 辅助色调
+val SecondaryPurple = Color(0xFF7C4DFF)
+val SecondaryGray = Color(0xFFF5F5F5)
+
+// 强调色
+val AccentOrange = Color(0xFFFFAB40)
+val AccentGreen = Color(0xFF00C853)
+
+// 文字颜色
+val TextDark = Color(0xFF212121)
+val TextWhite = Color(0xFFFFFFFF)
+val TextGray = Color(0xFF757575)
+
+
+// 背景色
+val BackgroundWhite = Color(0xFFFFFFFF)
+val BackgroundGray = Color(0xFFFAFAFA)
+var NavBackgroundWhite = Color(0xFFFFFFFF)
+
+// 状态色
+val StatusRed = Color(0xFFFF5252)
+val StatusGreen = Color(0xFF00C852)
+
+val WhiteWindow = Color(0xFFF5F6F7)
+val WhiteBackground = Color(0xFFFFFFFF)
+
+
+val BlackWindow = Color(0xFF111111)
+val BlackBackground = Color(0xFF1F1D1D)
