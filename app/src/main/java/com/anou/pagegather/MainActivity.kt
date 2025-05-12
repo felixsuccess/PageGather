bc938b021c34ef844260fc522cd78d0918fd30f2
ajf <ajf@anou.tech>
2025-05-12 15:39:37 +0800

    feat(ui): 实现沉浸式导航栏并优化主页面布局
    - 在 AndroidManifest.xml 中添加应用名称
    - 重构 MainActivity.kt，实现沉浸式导航栏
    - 更新 MainScreen.kt，优化底部导航栏样式
    - 新增 PageGatherApplication.kt 文件，初始化 Hilt

--- app/src/main/java/com/anou/pagegather/MainActivity.kt
+++ app/src/main/java/com/anou/pagegather/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.anou.pagegather
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.stringResource
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.anou.pagegather.ui.navigation.MainScreen
-import com.anou.pagegather.ui.navigation.Routes
 import com.anou.pagegather.ui.theme.PageGatherTheme
 import kotlinx.coroutines.delay
 
@@ -26,6 +42,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PageGatherTheme {
+                SetupImmersiveNavigation()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MainContent(
                         modifier = Modifier.padding(innerPadding), route = null
@@ -39,16 +56,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent(
-    modifier: Modifier = Modifier, route: String?
+    modifier: Modifier = Modifier, route: String?,
 ) {
-    val navController = rememberNavController()
     var isSplash = remember { mutableStateOf(true) }
     LaunchedEffect(Unit) {
         delay(2000) // 延迟 2 秒
         isSplash.value = false
     }
     if (isSplash.value) {
-        SplashScreen(onClick = { navController.navigate(Routes.MainRoutes.MAIN) })
+        SplashScreen()
     } else {
         MainScreen()
     }
@@ -56,12 +72,75 @@ fun MainContent(
 
 
 @Composable
-fun SplashScreen(onClick: () -> Unit) {
-    Text("捕捉字里行间的光")
+fun SplashScreen() {
+    val textToDisplay = stringResource(R.string.app_slogan)
+    val displayedTextLength = remember { Animatable(0f) }
 
-    Button(onClick = onClick) {
-
-        Text("出发")
+    LaunchedEffect(Unit) {
+        displayedTextLength.animateTo(
+            targetValue = textToDisplay.length.toFloat(),
+            animationSpec = tween(
+                durationMillis = 1500,
+                easing = LinearEasing
+            )
+        )
     }
 
+    val displayedText = textToDisplay.take(displayedTextLength.value.toInt())
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column {
+            displayedText.forEach { char ->
+                Text(
+                    char.toString(),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+    }
+
+}
+
+
+@SuppressLint("InlinedApi")
+@Composable
+fun SetupImmersiveNavigation() {
+    val view = LocalView.current
+    val isLightTheme = !isSystemInDarkTheme()
+    val window = (view.context as? ComponentActivity)?.window
+
+    SideEffect {
+        window?.let {
+            WindowCompat.setDecorFitsSystemWindows(it, false)
+            val controller = WindowInsetsControllerCompat(it, view)
+
+            // 兼容 Android 6.0 及以上设置状态栏图标颜色
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                controller.isAppearanceLightStatusBars = isLightTheme
+            }
+
+            // 兼容 Android 8.0 及以上设置导航栏图标颜色
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                controller.isAppearanceLightNavigationBars = isLightTheme
+            }
+
+            // 设置状态栏和导航栏透明
+            it.statusBarColor = Color.Transparent.toArgb()
+            it.navigationBarColor = Color.Transparent.toArgb()
+
+            // Android 11+ 隐藏状态栏和导航栏阴影
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                controller.systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }
+    }
 }
