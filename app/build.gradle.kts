a0986317bb7aabffd9426965f915e61283c399ca
ajf <ajf@anou.tech>
2025-05-12 14:20:14 +0800

    build: 集成 Room 和 Hilt 依赖
    - 在 app/build.gradle.kts 中添加 Room 和 Hilt 相关依赖
    - 在 build.gradle.kts 中添加 Hilt 插件和 KSP
    - 更新 gradle/libs.versions.toml，添加 Room 和 Hilt 版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -50,6 +52,15 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    //room
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    // hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    // Hilt Navigation Compose
+    implementation(libs.androidx.hilt.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
