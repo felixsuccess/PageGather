dc37229da49a5e753dbcd4702721bc7d477d9269
ajf <ajf@anou.tech>
2025-05-12 15:39:37 +0800

    feat(ui): 实现沉浸式导航栏并优化主页面布局
    - 在 AndroidManifest.xml 中添加应用名称
    - 重构 MainActivity.kt，实现沉浸式导航栏
    - 更新 MainScreen.kt，优化底部导航栏样式
    - 新增 PageGatherApplication.kt 文件，初始化 Hilt

--- app/src/main/java/com/anou/pagegather/ui/navigation/MainScreen.kt
+++ app/src/main/java/com/anou/pagegather/ui/navigation/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.anou.pagegather.ui.navigation
 
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
@@ -10,12 +12,17 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-
+import com.anou.pagegather.ui.theme.TextDark
+import com.anou.pagegather.ui.theme.TextGray
 // 主页面，包含底部导航
 @Composable
 fun MainScreen() {
@@ -32,7 +39,15 @@ fun MainScreen() {
 
 
     val showBottomBar = currentRoute in bottomBarRoutes
-
+    val selectedColor: Color = TextDark
+    val unselectedColor: Color = TextGray
+    val navColors=    NavigationBarItemDefaults.colors(
+        indicatorColor = Color.Transparent,
+        selectedIconColor = (selectedColor),
+        selectedTextColor = (selectedColor),
+        unselectedIconColor = (unselectedColor),
+        unselectedTextColor = (unselectedColor)
+    )
     Scaffold(
         bottomBar = {
             if (showBottomBar) {
@@ -40,29 +55,44 @@ fun MainScreen() {
                     NavigationBarItem(
                         selected = currentRoute == Routes.DashboardRoutes.STATISTICS,
                         onClick = { navController.navigate(Routes.DashboardRoutes.STATISTICS) },
-                        icon = { Icon(Icons.Default.DateRange, contentDescription = "统计") }
+                        icon = { Icon(Icons.Default.DateRange, contentDescription = "统计") },
+                        label = { Text(text = "统计", fontSize = 13.sp, lineHeight = 13.sp) },
+                        colors = navColors
+
                     )
                     NavigationBarItem(
                         selected = currentRoute == Routes.BookRoutes.BOOKS,
                         onClick = { navController.navigate(Routes.BookRoutes.BOOKS) },
-                        icon = { Icon(Icons.Default.Menu, contentDescription = "书架") }
+                        icon = { Icon(Icons.Default.Menu, contentDescription = "书架") },
+                        label = { Text(text = "书架", fontSize = 13.sp, lineHeight = 13.sp) },
+                        colors = navColors
                     )
 
                     NavigationBarItem(
                         selected = currentRoute == Routes.NoteRoutes.NOTE_LIST,
                         onClick = { navController.navigate(Routes.NoteRoutes.NOTE_LIST) },
-                        icon = { Icon(Icons.Default.Info, contentDescription = "随记") }
+                        icon = { Icon(Icons.Default.Info, contentDescription = "随记") },
+                        label = { Text(text = "随记", fontSize = 13.sp, lineHeight = 13.sp) },
+                        colors = navColors
                     )
 
                     NavigationBarItem(
                         selected = currentRoute == Routes.ProfileRoutes.PROFILE,
                         onClick = { navController.navigate(Routes.ProfileRoutes.PROFILE) },
-                        icon = { Icon(Icons.Default.Person, contentDescription = "我的") }
+                        icon = { Icon(Icons.Default.Person, contentDescription = "我的") },
+                        label = { Text(text = "我的", fontSize = 13.sp, lineHeight = 13.sp) },
+                        colors = navColors
                     )
                 }
             }
         }
     ) { padding ->
-        AppNavigation(navController = navController, modifier = Modifier.padding(padding))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+        ) {
+            AppNavigation(navController = navController)
+        }
     }
 }
